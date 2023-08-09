5a8160f901ba3b3f6c4e46d6dd9f333c386c6e26
paulmurdoch19 <80228075+paulmurdoch19@users.noreply.github.com>
2023-08-09 13:34:13 -0700

    tests are now working properly

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,31 +1,30 @@
 import pytest
-import requests
 
 from blogwebsite.app import app
 
 
 
-@pytest.fixture(scope='module')
-def start_app():
-    app.run(port=5000)
+@pytest.fixture()
+def client():
+    return app.test_client()
 
 # Test if the routes return expected responses
-def test_hello_world(start_app):
-    response = requests.get('http://127.0.0.1:5000/')
+def test_hello_world(client):
+    response = client.get('/')
     assert response.status_code == 200
     assert "<h1>Hello, World!</h1>" in response.text
 
-def test_about(start_app):
-    response = requests.get('http://127.0.0.1:5000/about')
+def test_about(client):
+    response = client.get('/about')
     assert response.status_code == 200
     assert "About Page" in response.text
 
-def test_home(start_app):
-    response = requests.get('http://127.0.0.1:5000/home')
+def test_home(client):
+    response = client.get('/home')
     assert response.status_code == 200
     assert "Home Page" in response.text
 
-def test_test(start_app):
-    response = requests.get('http://127.0.0.1:5000/test')
+def test_test(client):
+    response = client.get('/test')
     assert response.status_code == 200
     assert "this is a test route" in response.text
